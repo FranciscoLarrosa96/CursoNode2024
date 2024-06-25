997a4f389d9093028d71cef261a76aa6bbb8ef1d
Francisco Larrosa <franciscolarrosa22@gmail.com>
2024-06-25 10:00:25 -0300

    Desarrollando en Node - Variables de entorno por defecto

--- 02-Bases/src/app.js
+++ 02-Bases/src/app.js
@@ -1,3 +1,4 @@
 const {emailTemplate} =  require('./js-foundation/01-template');
+require('./js-foundation/02-destructuring');
 //Desestructuro la cosntante
 console.log(emailTemplate);
---
+++ 02-Bases/src/js-foundation/02-destructuring.js
@@ -0,0 +1,11 @@
+// console.log(process.env);
+
+// desestructuro
+const {USERDOMAIN,PUBLIC} = process.env;
+
+
+// console.table({USERDOMAIN,PUBLIC});
+
+const characters = ['Flash', 'Superman', 'Batman']
+const {2: bat} = characters
+// console.table(bat)
