d482c29ab6ffa4024a30bc239845659bd042d6c2
Francisco Larrosa <franciscolarrosa22@gmail.com>
2024-07-22 13:47:56 -0300

    Desarrollando en Node - Promesas

--- 02-Bases/src/app.js
+++ 02-Bases/src/app.js
@@ -1,25 +1,7 @@
-// const {emailTemplate} =  require('./js-foundation/01-template');
-// require('./js-foundation/02-destructuring');
-// const { getUserById } = require('./js-foundation/03-callbacks');
-// const { getUserById } = require('./js-foundation/04-arrow');
-const { getAge, getUuid } = require('../plugins');
-const { buildMakePerson } = require('./js-foundation/05-factory');
-//Desestructuro la cosntante
-// console.log(emailTemplate);
-
-// getUserById(1, (err, user) => {
-//     if (err) {
-//         throw new Error(err)
-//     }
-
-//     console.log("🚀 ~ user:", user);
-// });
-
-const makePerson = buildMakePerson({ getUuid, getAge });
-
-const obj = { name: 'Francisco', birthdate: '1996-05-10' };
+const {getPokemonById} = require('./js-foundation/06-promises');
 
 
-const fran = makePerson(obj);
-
-console.log("🚀 ~ obj:", { fran })
+getPokemonById(1)
+.then((pokemon) => {
+    console.log("🚀 ~ .then ~ pokemon:", pokemon)
+})
