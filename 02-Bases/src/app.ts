3a6ea924f8fce0933333cab3c4e9a27f6dbe9893
Francisco Larrosa <fran.7id@gmail.com>
2024-08-27 15:09:34 -0300

    Testing init

--- 02-Bases/src/app.ts
+++ 02-Bases/src/app.ts
@@ -1,17 +1,17 @@
-// // const {getPokemonById} = require('./js-foundation/06-promises');
-// const {buildLogger} = require('../plugins');
+// // // const {getPokemonById} = require('./js-foundation/06-promises');
+// // const {buildLogger} = require('../plugins');
 
-import { buildLogger } from "./plugins/logger.plugin";
-
-
+// import { buildLogger } from "./plugins/logger.plugin";
 
 
-const logger = buildLogger('app.js');
 
-logger.log('To Typescript');
-logger.error('Error To Typescript xd');
-// // getPokemonById(10)
-// // .then((pokemon) => {
-// //     console.log("🚀 ~ getPokemonById ~ pokemon:", pokemon)
-// // })
-// // .catch((err) => console.log('ERROR'))
+
+// const logger = buildLogger('app.js');
+
+// logger.log('To Typescript');
+// logger.error('Error To Typescript xd');
+// // // getPokemonById(10)
+// // // .then((pokemon) => {
+// // //     console.log("🚀 ~ getPokemonById ~ pokemon:", pokemon)
+// // // })
+// // // .catch((err) => console.log('ERROR'))
