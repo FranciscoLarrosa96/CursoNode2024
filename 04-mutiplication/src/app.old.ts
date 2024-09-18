96113f6d59b1d80ee1ef9428ba2c9f50a62aa6a3
Francisco Larrosa <fran.7id@gmail.com>
2024-09-18 10:09:08 -0300

    Clean architecture - Usando yarg empiricamente

--- 04-mutiplication/src/app.old.ts
+++ 04-mutiplication/src/app.old.ts
@@ -1,21 +1,24 @@
 import fs from 'fs';
+import { yarg } from './config/plugins/yargs.plugin';
 
-
+const { b: base, l: limit, s: show } = yarg;
 
 let outputMessage = '';
-const base = 5;
+
 const headerMessage = `
     ====================
       tabla del ${base}
     ====================
 `;
 
-for (let index = 1; index < 10; index++) {
-    outputMessage += `${base} x ${index} = ${base * index}\n`
+for (let index = 1; index < limit; index++) {
+  outputMessage += `${base} x ${index} = ${base * index}\n`
 }
 
 outputMessage = headerMessage + outputMessage;
-console.log("🚀 ~ outputMessage:", outputMessage)
+if (show) {
+  console.log("🚀 ~ outputMessage:", outputMessage)
+}
 
 const outputPath = 'outputs';
 
