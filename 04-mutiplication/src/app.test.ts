a60fa0f02ef38a4e266dc5a1f948187fe8e2911d
Francisco Larrosa <fran.7id@gmail.com>
2024-09-20 18:18:27 -0300

    Pruebas - App.ts

--- 04-mutiplication/src/app.test.ts
+++ 04-mutiplication/src/app.test.ts
@@ -1,5 +1,43 @@
-describe('App', () => {
-    test('Should be true', () => {
-        expect(true).toBe(true);
+import { ServerApp } from "./presentation/server-app";
+
+
+
+
+describe('Test App', () => {
+    test('Should call Server.run with values', async () => {
+
+        const serverRunMock = jest.fn();
+
+        ServerApp.run = serverRunMock;
+
+        process.argv =
+            [
+                'node',
+                'app.ts',
+                '-b',
+                '10',
+                '-l',
+                '5',
+                '-s',
+                '-n',
+                'test-file',
+                '-d',
+                'test-destination'
+            ];
+
+        await import('./app');
+
+
+            //No ejecuta la creacion de tabla ni archivo
+            // Solo verifica que ServerRun sea llamado con los argumentos
+        expect(serverRunMock).toHaveBeenCalledWith(
+            {
+                base: 10,
+                limit: 5,
+                showTable: true,
+                nameFile : 'test-file',
+                destinationFile: 'test-destination'
+            }
+        );
     });
 });
