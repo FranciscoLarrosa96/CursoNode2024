07441db9abc30a6d0d784f940e20e49bf3d3bea9
Francisco Larrosa <fran.7id@gmail.com>
2024-09-23 10:05:00 -0300

    CRON Task

--- 05-NOC/src/presentation/server.ts
+++ 05-NOC/src/presentation/server.ts
@@ -1,6 +1,15 @@
+import { CronJob } from "cron";
+
 
 export class Server {
     static start() {
         console.log('Server Started...');
+        const job = new CronJob(
+            '*/1 * * * * *', // cronTime
+            () => {
+                console.log('You will see this message every second');
+            },
+        );
+        job.start();
     }
 }
