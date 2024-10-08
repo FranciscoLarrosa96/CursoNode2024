f42351e620569d55c2f48b69029c93fec5698cba
Francisco Larrosa <fran.7id@gmail.com>
2024-10-08 09:09:48 -0300

    Responder demas archivos

--- 07-RESTWeb/src/app.ts
+++ 07-RESTWeb/src/app.ts
@@ -11,15 +11,21 @@ const server = http.createServer((req, res) => {
     // res.writeHead(200, { 'Content-Type': 'application/json' });
     // res.end(JSON.stringify(data));
 
-    if(req.url === '/'){
+    if (req.url === '/') {
         const htmlFile = fs.readFileSync('./public/index.html', 'utf8');
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(htmlFile);
-    }else{
-        res.writeHead(404, { 'Content-Type': 'text/html' });
-        res.end('<h1>404 Not Found</h1>');
+        return;
     }
 
+    if (req.url?.endsWith('.js')) {
+        res.writeHead(200, { 'Content-Type': 'application/javascript' });
+    } else if (req.url?.endsWith('.css')) {
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+    }
+
+    const responseContent = fs.readFileSync(`./public${req.url}`, 'utf8');
+    res.end(responseContent);
 });
 
 server.listen(8080, () => {
