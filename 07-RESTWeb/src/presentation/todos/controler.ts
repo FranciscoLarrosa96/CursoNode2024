204ec704713312927fab1b83e9137e6c2a5400cb
Francisco Larrosa <fran.7id@gmail.com>
2024-10-15 08:07:56 -0300

    CRUD - Read

--- 07-RESTWeb/src/presentation/todos/controler.ts
+++ 07-RESTWeb/src/presentation/todos/controler.ts
@@ -1,29 +1,43 @@
 import { Request, Response } from 'express';
 
+const todos = [
+    {
+        id: 1,
+        name: 'Buy Milk',
+        createAt: new Date()
+    },
+    {
+        id: 2,
+        name: 'Buy Bread',
+        createAt: new Date()
+    },
+    {
+        id: 3,
+        name: 'Buy Eggs',
+        createAt: new Date()
+    }
+]
+
 export class TodosController {
 
     constructor() { }
 
 
     public getTodos = (req: Request, res: Response) => {
-        res.json(
-            [
-                {
-                    id: 1,
-                    name: 'Buy Milk',
-                    createAt: new Date()
-                },
-                {
-                    id: 2,
-                    name: 'Buy Bread',
-                    createAt: new Date()
-                },
-                {
-                    id: 3,
-                    name: 'Buy Eggs',
-                    createAt: new Date()
-                }
-            ]
+        res.json(todos);
+    }
+
+    public getTodoById = (req: Request, res: Response) => {
+        const id = +req.params.id;
+        if (isNaN(id)) {
+            res.status(400).json(
+                { error: 'Invalid ID' }
+            );
+            return;
+        }
+        const todo = todos.find(todo => todo.id == id);
+        (todo) ? res.json(todo) : res.status(404).json(
+            { error: 'Todo not found' }
         );
     }
 }
