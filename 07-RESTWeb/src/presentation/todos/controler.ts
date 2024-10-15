9a37fa1be8481fa4f2d855e9682812c63476b1aa
Francisco Larrosa <fran.7id@gmail.com>
2024-10-15 10:01:05 -0300

    CRUD - Delete

--- 07-RESTWeb/src/presentation/todos/controler.ts
+++ 07-RESTWeb/src/presentation/todos/controler.ts
@@ -3,17 +3,17 @@ import { Request, Response } from 'express';
 const todos = [
     {
         id: 1,
-        name: 'Buy Milk',
+        text: 'Buy Milk',
         createAt: new Date()
     },
     {
         id: 2,
-        name: 'Buy Bread',
-        createAt: new Date()
+        text: 'Buy Bread',
+        createAt: null
     },
     {
         id: 3,
-        name: 'Buy Eggs',
+        text: 'Buy Eggs',
         createAt: new Date()
     }
 ]
@@ -42,19 +42,63 @@ export class TodosController {
     }
 
     public createTodo = (req: Request, res: Response) => {
-        const { name } = req.body;
-        if (!name) {
+        const { text } = req.body;
+        if (!text) {
             res.status(400).json(
-                { error: 'Name is required' }
+                { error: 'text is required' }
             );
             return;
         }
         const todo = {
             id: todos.length + 1,
-            name,
+            text,
             createAt: new Date()
         }
         todos.push(todo);
         res.status(201).json(todo);
+    }
+
+    public updateTodo = (req: Request, res: Response) => {
+        const id = +req.params.id;
+        if (isNaN(id)) {
+            res.status(400).json(
+                { error: 'Invalid ID' }
+            );
+            return;
+        }
+        const todo = todos.find(todo => todo.id == id);
+        if (!todo) {
+            res.status(404).json(
+                { error: 'Todo not found' }
+            );
+            return;
+        }
+        const { text, createAt } = req.body;
+
+        todo.text = text || todo.text;
+        (createAt === 'null')
+            ? todo.createAt = null
+            : todo.createAt = new Date(createAt || todo.createAt);
+
+        res.json(todo);
+    }
+
+    public deleteTodo = (req: Request, res: Response) => {
+        const id = +req.params.id;
+        if (isNaN(id)) {
+            res.status(400).json(
+                { error: 'Invalid ID' }
+            );
+            return;
+        }
+        const index = todos.findIndex(todo => todo.id == id);
+        if (index === -1) {
+            res.status(404).json(
+                { error: 'Todo not found' }
+            );
+            return;
+        }
+        todos.splice(index, 1);
+        res.status(200).send('Todo deleted');
     }
 }
