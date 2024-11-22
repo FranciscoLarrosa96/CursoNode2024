20ada0429460b1aacec25ad580a3b5ba9dee79c9
Francisco Larrosa <fran.7id@gmail.com>
2024-11-22 08:22:18 -0300

    User model

---
+++ 08-user-store/src/data/mongo/models/user.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email:
+    {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        validate: {
+            validator: (value: string) => {
+                return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(value);
+            },
+            message: (props: any) => `${props.value} is not a valid email address!`,
+        },
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    img: {
+        type: String,
+    },
+    role: {
+        type: [String],
+        default: ["USER"],
+        enum: ["ADMIN", "USER"],
+    }
+});
+
+export const User = mongoose.model("User", userSchema);
